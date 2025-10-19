5c6038188318724bad2aa7556aa679a097069ec3
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-10-19 20:23:57 +0500

    string functions

---
+++ chapter_03/str_functions.py
@@ -0,0 +1,9 @@
+name="pakistan zindabad"
+# len function
+print(len(name))
+# upper case function
+print(name.upper())
+# lower case function
+print(name.lower())
+# title function
+print(name.title())
