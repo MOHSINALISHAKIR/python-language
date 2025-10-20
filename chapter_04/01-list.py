3ac52ccd6dfc43d43b7913122b6cb3ccdc0fbd1f
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-10-20 21:02:56 +0500

    list and method

---
+++ chapter_04/01-list.py
@@ -0,0 +1,6 @@
+friends = ["uznain","mohsin",23,78.9]
+# we can use different types of data types in list 
+#  list are mutable means we can change the list
+print(friends[0])
+friends[0]="saad"
+print(friends[0])
---
+++ chapter_04/02-listMethod.py
@@ -0,0 +1,21 @@
+friends=["faizan","saad","ali","uznain","mohsin","anas"]
+# Methods
+# append method
+friends.append("raza")
+print(friends)
+# sort method
+l1=[2,1,3,4,5,7,6]
+l1.sort()
+print(l1)
+# reverse method
+l1.reverse()
+print(l1)
+# insert method
+l1.insert(1,200)
+print(l1)
+# pop method
+l1.pop(1)
+print(l1)
+# remove method
+l1.remove(7)
+print(l1)
